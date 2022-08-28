9e862ac2e7c24876780a9f138a8227748e7fa30b
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-28 16:54:22 +0600

    Upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./App.scss";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
