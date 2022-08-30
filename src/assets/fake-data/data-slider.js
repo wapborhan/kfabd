97da8ee9e1fd4142fd465c28aabd9991f0dc48cf
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-30 11:46:24 +0600

    Update data-slider.js

--- src/assets/fake-data/data-slider.js
+++ src/assets/fake-data/data-slider.js
@@ -1,45 +1,46 @@
-import bgImg1 from '../images/background/bg-1.jpg';
-import bgImg2 from '../images/background/bg-3.jpg';
-import bgImg3 from '../images/background/bg-4.jpg';
-import img1 from '../images/common/itemslider.png';
+import bgImg1 from "../images/background/bg-1.jpg";
+import bgImg2 from "../images/background/bg-3.jpg";
+import bgImg3 from "../images/background/bg-4.jpg";
+import img1 from "../images/common/itemslider.png";
+import img2 from "../images/common/itemslider1.png";
 
 const dataSlider = [
-    {
-        id: 1,
-        title: 'CYbox nft collectionS for everyone',
-        desc : 'Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.',
-        bgImg: bgImg1,
-        img : img1
-    },
-    {
-        id: 2,
-        title: 'CYbox nft collectionS for everyone',
-        desc : 'Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.',
-        bgImg: bgImg2,
-        classAction: 'two'
-    },
-    {
-        id: 3,
-        title: 'CYbox nft collectionS for everyone',
-        desc : 'Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.',
-        bgImg: bgImg3,
-        classAction: 'three'
-    },
-    {
-        id: 4,
-        title: 'CYbox nft collectionS for everyone',
-        desc : 'Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.',
-        bgImg: bgImg1,
-        img : img1
-    },
-    {
-        id: 5,
-        title: 'CYbox nft collectionS for everyone',
-        desc : 'Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.',
-        bgImg: bgImg2,
-        classAction: 'two'
-    },
-
-]
+  {
+    id: 1,
+    title: "CYbox nft collectionS for everyone",
+    desc: "Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.",
+    bgImg: bgImg1,
+    img: img1,
+  },
+  {
+    id: 2,
+    title: "CYbox nft collectionS for everyone",
+    desc: "Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.",
+    bgImg: bgImg1,
+    classAction: "two",
+    img: img1,
+  },
+  {
+    id: 3,
+    title: "CYbox nft collectionS for everyone",
+    desc: "Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.",
+    bgImg: bgImg1,
+    classAction: "three",
+  },
+  {
+    id: 4,
+    title: "CYbox nft collectionS for everyone",
+    desc: "Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.",
+    bgImg: bgImg1,
+    img: img1,
+  },
+  {
+    id: 5,
+    title: "CYbox nft collectionS for everyone",
+    desc: "Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.",
+    bgImg: bgImg1,
+    classAction: "two",
+  },
+];
 
 export default dataSlider;
